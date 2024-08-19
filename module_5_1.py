ef91a901b0e087b55acb1e066c71cd75a1eaf6d8
MakashovAV <mak2440@yandex.ru>
2024-08-19 18:53:03 +0700

    Цель: применить на практике знания о классах, объектах и их атрибутах.

--- module_5_1.py
+++ module_5_1.py
@@ -7,7 +7,7 @@ class House:
         self.number_of_floors = floors
 
     def go_to(self, new_floor):
-        if new_floor > self.number_of_floors:
+        if new_floor > self.number_of_floors and new_floor > 0:
             print("Такого этажа не существует")
         else:
             for i in range(1, new_floor + 1):
@@ -17,6 +17,6 @@ class House:
 h1 = House('ЖК Горский', 18)
 h2 = House('Домик в деревне', 2)
 h3 = House('ЖК Эльбрус', 10)
-h1.go_to(5)
+h1.go_to(0)
 h2.go_to(10)
 h3.go_to(3)

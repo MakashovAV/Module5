dc80f45dedcb934f64c9b4b0353b66a348fc7ea4
MakashovAV <mak2440@yandex.ru>
2024-08-19 18:28:10 +0700

    Цель: применить на практике знания о классах, объектах и их атрибутах.

---
+++ module_5_1.py
@@ -0,0 +1,22 @@
+# Цель: применить на практике знания о классах, объектах и их атрибутах.
+# Задача "Developer - не только разработчик":
+
+class House:
+    def __init__(self, name, floors):
+        self.name = name
+        self.number_of_floors = floors
+
+    def go_to(self, new_floor):
+        if new_floor > self.number_of_floors:
+            print("Такого этажа не существует")
+        else:
+            for i in range(1, new_floor + 1 ):
+                print(i)
+
+
+h1 = House('ЖК Горский', 18)
+h2 = House('Домик в деревне', 2)
+h3 = House('ЖК Эльбрус', 10)
+h1.go_to(5)
+h2.go_to(10)
+h3.go_to(3)

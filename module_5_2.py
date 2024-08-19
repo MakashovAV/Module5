08670557c87eb55e2adda426b3e3ee7740a65229
MakashovAV <mak2440@yandex.ru>
2024-08-19 18:50:19 +0700

    Цель: понять как работают базовые магические методы на практике.

--- module_5_2.py
+++ module_5_2.py
@@ -14,10 +14,10 @@ class House:
                 print(i)
 
     def __len__(self):
-        return (self.number_of_floors)
+        return self.number_of_floors
 
     def __str__(self):
-        return (f'Название: {self.name}, кол-во этажей:{self.number_of_floors}')
+        return f'Название: {self.name}, кол-во этажей:{self.number_of_floors}'
 
 
 h1 = House('ЖК Эльбрус', 10)
